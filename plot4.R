a01df89df395dffb40ea0d5a6c84998111e5ef53
Christian von Uffel <christian.uffel@gmail.com>
2014-11-12 14:08:48 -0500

    Plot 4
    Reviewed code and found new methods to create plots that are less
    convoluted and achieve the stated goals

--- plot4.R
+++ plot4.R
@@ -1,33 +1,55 @@
-## Get full dataset
-data_full <- read.csv("/Users/Christian/Documents/R/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+# File in WD
 
-## Subset the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
+# Convert to Table(Easy way)
+header <- as.vector(t(read.table("household_power_consumption.txt", 
+                                 sep = ";", 
+                                 nrows = 1)))
 
-## Convert dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+mydata <- read.table("household_power_consumption.txt", 
+                     sep = ";", 
+                     col.names = header, 
+                     nrows = 2880, 
+                     skip = 66637)
 
-## Create plot
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(data, {
-    plot(Global_active_power~Datetime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    plot(Voltage~Datetime, type="l", 
-         ylab="Voltage (volt)", xlab="")
-    plot(Sub_metering_1~Datetime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~Datetime,col='Red')
-    lines(Sub_metering_3~Datetime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power~Datetime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
-})
+# Convert Dates
+mydata$DateTime <- strptime(paste(mydata$Date, mydata$Time), "%d/%m/%Y %H:%M:%S")
 
-## Save image
-dev.copy(png, file="plot4.png", height=480, width=480)
+par(mfrow = c(2,2))
+
+# Plot 1
+plot(mydata$DateTime, mydata$Global_active_power,
+     type="l", #type = line
+     xlab="", #x-axis label
+     ylab="Global active power", #y-axis label
+     col="black") #color
+
+# Plot 2
+plot(mydata$DateTime, mydata$Voltage,
+     type="l", #type = line
+     xlab="datetime", #x-axis label
+     ylab="Voltage", #y-axis label
+     col="black") #color
+
+# Plot 3
+plot(mydata$DateTime, mydata$Sub_metering_1,
+     type="l", #type = line
+     xlab="", #x-axis label
+     ylab="Energy sub metering", #y-axis label
+     col="black") #color
+points(mydata$DateTime, mydata$Sub_metering_2, type = "l", col = "red")
+points(mydata$DateTime, mydata$Sub_metering_3, type = "l", col = "blue")
+legend( x="topright",
+        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+        col=c("black","red","blue"), 
+        lty=c(1,1,1))
+
+# Plot 4
+plot(mydata$DateTime, mydata$Global_reactive_power,
+     type="l", #type = line
+     xlab="datetime", #x-axis label
+     ylab="Voltage", #y-axis label
+     col="black") #color
+
+# Save png Image file
+dev.copy(png, file = "plot4.png")
 dev.off()
